d42c8e059c204df2f7176ae777a1d23f32223687
Romario Huebra <romarioj2h@gmail.com>
2021-02-01 22:16:34 -0300

    Removing unneeded package

--- src/config.js
+++ src/config.js
@@ -1,5 +1,3 @@
-require('dotenv').config()
-
 module.exports = {
     db: {
         user: process.env.DB_USER,
--- src/index.js
+++ src/index.js
@@ -1,23 +1,20 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 const routes = require('./routes/index')
 const passport = require('passport')
 const morgan = require('morgan')
-const flash = require('connect-flash')
 const passportService = require('./services/passport')
-require('dotenv').config()
 
 passport.use(passportService.strategy)
 passport.serializeUser(passportService.serializeUser)
 passport.deserializeUser(passportService.deserializeUser)
 
 app.use(passport.initialize())
-app.use(passport.session())
 app.use(morgan('dev'))
 app.use(express.json())
 app.use('/', routes)
-app.use(flash())
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
