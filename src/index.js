286ef6e30b407959f589640900155650fcb5c020
Romario Huebra <romarioj2h@gmail.com>
2021-02-01 22:07:41 -0300

    Creating passport service

--- src/index.js
+++ src/index.js
@@ -3,40 +3,14 @@ const app = express()
 const port = process.env.PORT || 3000
 const routes = require('./routes/index')
 const passport = require('passport')
-const JwtStrategy = require('passport-jwt').Strategy
-const ExtractJwt = require('passport-jwt').ExtractJwt
-const UserModel = require('./models/user')
 const morgan = require('morgan')
 const flash = require('connect-flash')
+const passportService = require('./services/passport')
 require('dotenv').config()
 
-passport.use(new JwtStrategy(
-    {
-        jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-        secretOrKey: process.env.JWT_SECRET
-    },
-    function (jwt_payload, done) {
-        UserModel.findOne(
-            {
-                where: {
-                    id: jwt_payload.id
-                }
-            }
-        ).then((user) => {
-            if (user) {
-                return done(null, user);
-            }
-            return done(null, false);
-        });
-    }
-))
-
-passport.serializeUser(function (user, done) {
-    done(null, user);
-})
-passport.deserializeUser(function (user, done) {
-    done(null, user);
-})
+passport.use(passportService.strategy)
+passport.serializeUser(passportService.serializeUser)
+passport.deserializeUser(passportService.deserializeUser)
 
 app.use(passport.initialize())
 app.use(passport.session())
